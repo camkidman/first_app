c0c34ebb2bfeefdef3b29cb8414f4aef5b10ac9d
Cameron Kidman <cameronjkidman@gmail.com>
2013-11-30 16:18:42 -0700

    Initial commit of first_app, hartl tutorial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FirstApp::Application.config.secret_key_base = 'e894a08f7111532cbde1e5bc440f379784c73fe5b34bc0f2a07164f4646c975c917a64ad603c73a4460098cfd306d033883b414fd0d5d5ae486cee4c5beae314'
